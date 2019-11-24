89bd1b876fde99f3b38fbf1508a22b91f0f83f95
Nobuyoshi Shimmen <nobuyoshi.shimmen@gmail.com>
2019-11-24 11:59:32 +0900

    circleci修正

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,14 @@
-module.exports = {
-  configureWebpack: {
-    // plugins: [
-    //   new MyAwesomeWebpackPlugin()
-    // ],
-    // resolve: {
-    //   extensions: ['.js', '.vue'],
-    // },
-  },
-  // devServer: {
-  //   host: 'local.q-torial.com',
-  //   https: true,
-  // },
-};
+// module.exports = {
+//   configureWebpack: {
+//     plugins: [
+//       new MyAwesomeWebpackPlugin()
+//     ],
+//     resolve: {
+//       extensions: ['.js', '.vue'],
+//     },
+//   },
+//   devServer: {
+//     host: 'local.q-torial.com',
+//     https: true,
+//   },
+// };
