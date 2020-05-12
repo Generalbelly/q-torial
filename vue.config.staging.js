7fc5d43858494d70a43a0db9f8efa50be27ca88c
Nobuyoshi Shimmen <nobuyoshi.shimmen@gmail.com>
2020-05-12 22:40:03 +0900

    descriptionタグを追加

--- vue.config.staging.js
+++ vue.config.staging.js
@@ -3,7 +3,6 @@ module.exports = {
     config
       .plugin('html')
       .tap(args => {
-        args[0].title = 'Qtorial';
         args[0].googleAnalyticsScript = `<!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-71759040-1"></script>
         <script>
