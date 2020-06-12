e7e272940b5759634f0cf52ed6893174315fca7c
Nobuyoshi Shimmen <nobuyoshi.shimmen@gmail.com>
2020-06-12 21:28:12 +0900

    修正

--- functions/src/models/task.ts
+++ functions/src/models/task.ts
@@ -3,6 +3,8 @@ import Model from './model';
 export default class Task extends Model {
   name: string = '';
 
+  payload: string = '';
+
   constructor(init?: Partial<Task>) {
     super();
     Object.assign(this, init);
